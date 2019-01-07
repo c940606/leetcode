c4258d7231f6c60a68dcffe31ab7ff8b3869a05d
caiwei <762307667@qq.com>
2019-01-07 21:36:28 +0800

    更新

--- 725. Split Linked List in Parts.py
+++ 725. Split Linked List in Parts.py
@@ -11,4 +11,5 @@ class Solution(object):
 		:type k: int
 		:rtype: List[ListNode]
 		"""
+		
 
--- 792. Number of Matching Subsequences.py
+++ 792. Number of Matching Subsequences.py
@@ -1,46 +1,46 @@
 class Solution(object):
-	def numMatchingSubseq(self, S, words):
-		"""
-		:type S: str
-		:type words: List[str]
-		:rtype: int
-		"""
-		from collections import Counter
-		count = 0
-		words = Counter(words)
-		for word,num in words.items():
-			tmp = S
-			flag = False
-			for alp in word:
-				idx = tmp.find(alp)
-				if idx == -1:
-					flag = True
-					break
-				else:
-					tmp = tmp[idx + 1:]
-			if not flag:
-				count += num
-		return count
+    def numMatchingSubseq(self, S, words):
+        """
+        :type S: str
+        :type words: List[str]
+        :rtype: int
+        """
+        from collections import Counter
+        count = 0
+        words = Counter(words)
+        for word, num in words.items():
+            tmp = S
+            flag = False
+            for alp in word:
+                idx = tmp.find(alp)
+                if idx == -1:
+                    flag = True
+                    break
+                else:
+                    tmp = tmp[idx + 1:]
+            if not flag:
+                count += num
+        return count
 
-	def numMatchingSubseq1(self, S, words):
-		from collections import defaultdict,Counter
+    def numMatchingSubseq1(self, S, words):
+        from collections import defaultdict, Counter
 
-		import bisect
-		def isMatch(word, w_i, d_i):
-			if w_i == len(word): return True
-			l = dict_idxs[word[w_i]]
-			if len(l) == 0 or d_i > l[-1]: return False
-			i = l[bisect.bisect_left(l, d_i)]
+        import bisect
+        def isMatch(word, w_i, d_i):
+            if w_i == len(word): return True
+            l = dict_idxs[word[w_i]]
+            if len(l) == 0 or d_i > l[-1]: return False
+            i = l[bisect.bisect_left(l, d_i)]
 
-			# i = l[l.index(d_i)]
-			# print(i)
-			return isMatch(word, w_i + 1, i + 1)
+            # i = l[l.index(d_i)]
+            # print(i)
+            return isMatch(word, w_i + 1, i + 1)
 
-		dict_idxs = defaultdict(list)
-		print(Counter(words))
-		for i in range(len(S)):
-			dict_idxs[S[i]].append(i)
-		return sum(isMatch(word, 0, 0) for word in words)
+        dict_idxs = defaultdict(list)
+        print(Counter(words))
+        for i in range(len(S)):
+            dict_idxs[S[i]].append(i)
+        return sum(isMatch(word, 0, 0) for word in words)
 
 
 a = Solution()
